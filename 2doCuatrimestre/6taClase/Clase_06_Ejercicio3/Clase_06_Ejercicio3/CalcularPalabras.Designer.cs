8c48ef3f5b006e1d9c457971ab1110df8c074bbb
AgustinRguez <agus_rodriguez-@hotmail.com>
2021-09-13 13:25:53 -0300

    6ta clase ejercicio 3

---
+++ 2doCuatrimestre/6taClase/Clase_06_Ejercicio3/Clase_06_Ejercicio3/CalcularPalabras.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Clase_06_Ejercicio3
+{
+    partial class CalcularPalabras
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rchContenedorDeTexto = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rchContenedorDeTexto
+            // 
+            this.rchContenedorDeTexto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rchContenedorDeTexto.Location = new System.Drawing.Point(2, -1);
+            this.rchContenedorDeTexto.Name = "rchContenedorDeTexto";
+            this.rchContenedorDeTexto.Size = new System.Drawing.Size(797, 410);
+            this.rchContenedorDeTexto.TabIndex = 0;
+            this.rchContenedorDeTexto.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(713, 415);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // CalcularPalabras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rchContenedorDeTexto);
+            this.Name = "CalcularPalabras";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rchContenedorDeTexto;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ 2doCuatrimestre/6taClase/Clase_06_Ejercicio3/Clase_06_Ejercicio3/CalcularPalabras.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_06_Ejercicio3
+{
+    public partial class CalcularPalabras : Form
+    {
+        public CalcularPalabras()
+        {   
+            InitializeComponent();   
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> diccionario = ObtenerContadorPalabras();
+            List<KeyValuePair<string, int>> podio = diccionario.ToList();
+            podio.Sort(CompararCantidad);
+            MostrarPodio(podio);
+        }
+        private void MostrarPodio(List<KeyValuePair<string, int>> podio)
+        {
+            StringBuilder sb = new StringBuilder();
+            if(podio.Count == 0)
+            {
+                sb.AppendLine("No se ingresron palabras");
+            }
+            else
+            {
+                foreach (KeyValuePair<string, int> item in podio)
+                {
+                    sb.AppendLine($"Palabra: {item.Key} - Cantidad: {item.Value}");
+                }
+            }
+            
+            MessageBox.Show(sb.ToString(), "Podio");
+        }
+        private int CompararCantidad(KeyValuePair<string,int> primerElemento, KeyValuePair<string, int> segundoElemento)
+        {
+            return segundoElemento.Value - primerElemento.Value;
+            
+        }
+        private Dictionary<string,int> ObtenerContadorPalabras()
+        { 
+            string texto = rchContenedorDeTexto.Text;
+            Dictionary<string, int> diccionario = new Dictionary<string, int>();
+            string[] palabras = texto.Split(' ', StringSplitOptions.RemoveEmptyEntries); //el split separa el string en x numeros depende el caracter separador
+            foreach (string palabra in palabras)
+            {
+                if (diccionario.ContainsKey(palabra))
+                {
+                    diccionario[palabra]++;
+                }
+                else
+                {
+                    diccionario.Add(palabra, 1);
+                }
+            }
+            return diccionario;
+        }
+    }
+}
