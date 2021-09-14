dde224e93a75e7986b08516b5d5d245f1dcb2a68
AgustinRguez <agus_rodriguez-@hotmail.com>
2021-09-13 22:07:59 -0300

    Clase 7 ejercicio 1

---
+++ 2doCuatrimestre/7maClase/Clase_07_Ejercicio1/Clase_07_Ejercicio1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Entidades;
+namespace Clase_07_Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente c1 = new Cliente(1,"YO");
+            Cliente c2 = new Cliente(2,"SUPER YO");
+            Cliente c3 = new Cliente(3,"ULTRA BALL (?");
+            Negocio n1 = new Negocio("LALA");
+            if (n1 + c1)
+            {
+                Console.WriteLine("Se agrego c1");
+            }
+            if (!(n1 + c1))
+            {
+                Console.WriteLine("c1 ya esta");
+            }
+            if (n1 + c2)
+            {
+                Console.WriteLine("Se agrego c2");
+            }
+            if (n1 + c3)
+            {
+                Console.WriteLine("Se agrego c3");
+            }
+            Console.WriteLine("Clientes pendientes: {0}", n1.ClientePendiente);
+            while (~n1)
+            {
+                Console.WriteLine("Clientes pendientes: {0}", n1.ClientePendiente);
+            }
+        }
+    }
+}
---
+++ 2doCuatrimestre/7maClase/Clase_07_Ejercicio1/Entidades/PuestoAtencion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Entidades
+{
+    public class PuestoAtencion
+    {
+        public enum Puesto
+        {
+            Caja1,Caja2
+        }
+        private static int numeroActual;
+        private Puesto puesto;
+
+        static PuestoAtencion()
+        {
+            PuestoAtencion.numeroActual = 0;
+        }
+        public PuestoAtencion(Puesto puesto)
+        {
+            this.puesto = puesto;
+        }
+        public bool Atender(Cliente cli)
+        {
+            if(!(cli is null))
+            {
+                Thread.Sleep(2000);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static int NumeroActual
+        {
+            get
+            {
+                PuestoAtencion.numeroActual ++;
+                return numeroActual;
+            }
+        }
+    }
+}
