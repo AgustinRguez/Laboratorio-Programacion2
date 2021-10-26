1f35acc1e1dadf0167f8e8881bcaf7659350acc2
AgustinRguez <agus_rodriguez-@hotmail.com>
2021-10-25 21:43:37 -0300

    Ejercicio 1 incompleto

---
+++ 2doCuatrimestre/Clase_15_Ejercicio1/Clase_15_Ejercicio1/FrmAltaModificacion.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Clase_15_Ejercicio1
+{
+    partial class FrmAltaModificacion
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtObjeto = new System.Windows.Forms.TextBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtObjeto
+            // 
+            this.txtObjeto.Location = new System.Drawing.Point(76, 34);
+            this.txtObjeto.MaxLength = 50;
+            this.txtObjeto.Name = "txtObjeto";
+            this.txtObjeto.Size = new System.Drawing.Size(304, 23);
+            this.txtObjeto.TabIndex = 0;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(94, 79);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(131, 38);
+            this.btnConfirmar.TabIndex = 1;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(249, 79);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(131, 38);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // FrmAltaModificacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 144);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.txtObjeto);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmAltaModificacion";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtObjeto;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ 2doCuatrimestre/Clase_15_Ejercicio1/Clase_15_Ejercicio1/FrmAltaModificacion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_15_Ejercicio1
+{
+    public partial class FrmAltaModificacion : Form
+    {
+        private string tituloForm;
+        private string textoTxtObjeto;
+        private string textoBtnConfirmar;
+        public FrmAltaModificacion(string tituloForm, string textoTxtObjeto, string textoBtnConfirmar)
+        {
+            InitializeComponent();
+            this.tituloForm = tituloForm;
+            this.textoTxtObjeto = textoTxtObjeto;
+            this.textoBtnConfirmar = textoBtnConfirmar;
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            if (txtObjeto.Text is not null)
+            {
+                MessageBox.Show("El texto no tiene que estar vacío", "Texto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
